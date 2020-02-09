61c38df20e0fb569165c7bb465e5d1ecbbad116d
Thiha <aungthiha91027@gmail.com>
2020-02-09 18:10:08 +0630

    initial Structuring

---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Api from '../../components/Api.svelte'
+</script>
+
+<Api url="http://localhost:5000/users" method="get" let:data let:loading let:error>
+{#if data}
+<ul>
+    {#each data as user}
+    <li>{user.name}</li>
+    {/each}
+</ul>
+{:else if loading}
+<h1>loading</h1>
+{:else if error}
+<h1>{error}</h1>
+{/if}
+</Api>
+
+
+<style></style>
